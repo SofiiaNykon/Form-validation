e01a8de6ee91a1a445e48a2e43b575df06483373
SofiiaNykon <sofianikonovic@gmail.com>
2024-06-27 13:35:04 +0200

    + form validation

--- script.js
+++ script.js
@@ -6,13 +6,62 @@ for (const item of inputFields) {
     validateForm(event);
   });
 }
+const setError=(element, message)=>{
+    const errorSection = element.parentElement.querySelector(".error");
+    errorSection.innerText = message;
+    element.classList.add("invalid");
+    element.classList.remove("valid");
+};
+const setValid =element=>{
+    const errorSection = element.parentElement.querySelector(".error");
+    errorSection.innerText = "";
+    element.classList.remove("invalid");
+    element.classList.add("valid");
+}
 
+const validateEmail =(email)=>{
+    const regexEmail = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    if(email.value ===""){
+        setError(email, "Email is required");
+    }else if(!regexEmail.test(email.value)){
+        setError(email, "Email is incorrect");
+    }else{
+        setValid(email);
+    }
+}
 
-// required.value = ^[a-z0-9]/@[a-z]\.[a-z]{2,3}$;
-// email.onclick (){
-//     if(email === required){
-//         email.className = "valid";
-//     }else{
-//         email.className = "error" 
-//     }
-// }
+const validatePassword =(password)=>{
+    const  regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    if(password.value===""){
+        setError(password, "Email is required");
+    }else if(!regexPassword.test(password.value)){
+        setError(password, "Password must contain at least 8 symbols, one digit and one special character");
+    }else setValid(password);
+}
+
+const validatePasswordConfirm = (passwordConfirm) => {
+    if (passwordConfirm.value === "") {
+      setError(passwordConfirm, "Confirmation password is required");
+    } else if (passwordConfirm.value !== password.value) {
+      setError(passwordConfirm, "Password doesn't match!");
+    } else {
+      setValid(passwordConfirm);
+    }
+  };
+
+const validateForm = (event) =>{
+    switch(event.target.id){
+        case "email":
+            validateEmail(event.target);
+            break;
+            case "password":
+                validatePassword(event.target);
+                break;
+                case "passwordConfirm":
+                    validatePasswordConfirm(event.target);
+                    break;
+                    default:
+                        alert("Validation error!")
+
+    }
+};
